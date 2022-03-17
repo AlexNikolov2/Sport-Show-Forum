706e7e4364ab5d6dc0f99a668acd99a742bf0376
Aleks Nikolov <tcr.official.world@gmail.com>
2022-03-17 20:20:36 +0200

    header building, only design

--- sport-show-forum/src/app/app.module.ts
+++ sport-show-forum/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { CoreComponent } from './core/core.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoreComponent,
   ],
   imports: [
     BrowserModule,
