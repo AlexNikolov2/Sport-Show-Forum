43dc4ac84544f99ca7179be006034b4c3d5f0021
Aleks Nikolov <tcr.official.world@gmail.com>
2022-03-17 19:51:01 +0200

    build

--- sport-show-forum/src/app/app.module.ts
+++ sport-show-forum/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { CoreComponent } from './core/core.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CoreComponent,
   ],
   imports: [
     BrowserModule,
