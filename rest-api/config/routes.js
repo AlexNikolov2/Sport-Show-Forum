877763c42c67a70620585c9daebbaab4fd866c80
Aleks Nikolov <tcr.official.world@gmail.com>
2022-03-22 15:54:50 +0200

    build

--- rest-api/config/routes.js
+++ rest-api/config/routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 
 const authController = require('../controllers/auth');
+const postController = require('../controllers/post');
 
 const router = Router();
 
 router.use('/user', authController);
+router.use('/post', postController);
 
 module.exports = router;
