8c4b8611d781e46d88d767fd20b603bd817fc539
Aleks Nikolov <tcr.official.world@gmail.com>
2022-03-24 19:46:18 +0200

    build

--- sport-show-forum/src/app/user/user.module.ts
+++ sport-show-forum/src/app/user/user.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { ProfileComponent } from './profile/profile.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
-    LoginComponent,
-    RegisterComponent
   ],
   imports: [
     CommonModule,
