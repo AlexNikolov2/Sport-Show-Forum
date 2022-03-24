0d385a87672358fc4388df7c5bee0f30a92ab67e
Aleks Nikolov <tcr.official.world@gmail.com>
2022-03-24 19:55:59 +0200

    build

---
+++ sport-show-forum/src/app/post/post.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostDetailsComponent } from './post-details/post-details.component';
+import { PostsComponent } from './posts/posts.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
+
+
+
+@NgModule({
+  declarations: [
+    PostDetailsComponent,
+    PostsComponent,
+    CreatePostComponent,
+    EditPostComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PostModule { }
