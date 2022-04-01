9c4abbc694c9dc030518baf10a65ada481ff76da
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-02 00:45:20 +0300

    build

--- rest-api/controllers/auth.js
+++ rest-api/controllers/auth.js
@@ -1,2 +1,36 @@
 const router = require('express').Router();
 const api = require('../services/auth');
+const {cookie_name} = require('../config');
+
+router.post('/register', async(req, res) => {
+    try{
+        const user = await api.register(req.body);
+        res.status(200).send(user);
+    }
+    catch(err){
+        const error = mapErrors(err);
+        res.status(400).send(error);
+    }
+});
+
+router.post('/login', async(req, res) => {
+    try{
+        const user = await api.login(req.body);
+        res.status(200).send(user);
+    }
+    catch(err){
+        const error = mapErrors(err);
+        res.status(400).send(error);
+    }
+});
+
+router.get('/logout', async(req, res) => {
+    try{
+        res.clearCookie(cookie_name);
+        res.status(204).send({message: 'User is logged out'});
+    }
+    catch(err){
+        const error = mapErrors(err);
+        res.status(400).send(error);
+    }
+});
