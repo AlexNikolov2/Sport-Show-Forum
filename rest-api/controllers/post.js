ec669397bdccdb249ca8cce882db7243e0965ec2
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-01 15:51:37 +0300

    build

---
+++ rest-api/controllers/post.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const api = require('../services/post')
+const {isUser, isCreator} = require('../middlewares/guards');
+//mapper
+//preload
+
+router.get('/', async(req, res) => {
+    const posts = await api.getAll();
+    res.json(posts);
+});
--- rest-api/middlewares/guards.js
+++ rest-api/middlewares/guards.js
@@ -1,7 +1,27 @@
 const {getUserById} = require('../services/user'); 
 
+function isUser() {
+    return function (req, res, next) {
+        if (req.user) {
+            next();
+        } else {
+            res.status(401).json({message: 'Log in ffs'});
+        }
+    };
+}
+
+function isGuest() {
+    return function (req, res, next) {
+        if (!req.user) {
+            next();
+        } else {
+            res.status(400).json({message: 'You are already logged in'});
+        }
+    };
+}
+
 async function isCreator(req, res, next){
-    const user = await getUserById(req.user.id);
+    const user = await getUserById(req.user._id);
     if(user.id !== req.params.id){
         return res.status(403).json({
             message: 'You are not allowed to do this action'
@@ -12,7 +32,7 @@ async function isCreator(req, res, next){
 
 
 module.exports = {
-    isAuth,
+    isUser,
     isGuest,
     isCreator
 };
