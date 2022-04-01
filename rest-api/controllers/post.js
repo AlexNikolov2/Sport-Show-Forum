5dfe642145ba83cfe99a04359edbed4b6081e23a
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-01 16:58:49 +0300

    build

--- rest-api/controllers/post.js
+++ rest-api/controllers/post.js
@@ -8,3 +8,16 @@ router.get('/', async(req, res) => {
     const posts = await api.getAll();
     res.json(posts);
 });
+
+router.get('/:postId', async(req, res) => {
+    const post = await api.getById(req.params.id);
+    res.json(post);
+});
+
+router.get('/:postId/comments', async(req, res) => {
+    const comments = await api.getComments(req.params.id);
+    res.json(comments);
+});
+
+
+
--- rest-api/models/Post.js
+++ rest-api/models/Post.js
@@ -1,15 +1,22 @@
-const {model, Schema, Types: {ObjectId} } = require('mongoose');
+const {
+  model,
+  Schema,
+  Types: { ObjectId },
+} = require("mongoose");
 
-const postSchema = new Schema({ 
-    keyword: {type: String, required: [true, 'Keyword is required']},
-    title: {type: String, required: [true, 'Title is required']},
-    content: {type: String, required: [true, 'Content is required']},
-    img: {type: String, required: [true, 'Img is required']},
-    owner: {type: ObjectId, ref: 'User'},
-    likes: [{type: ObjectId, ref: 'User'}],
-    comments: [{type: ObjectId, ref: 'Comment'}],
-}, {timestamps: {createdAt: 'created_at'}});
+const postSchema = new Schema(
+  {
+    keyword: { type: String, required: [true, "Keyword is required"] },
+    title: { type: String, required: [true, "Title is required"] },
+    content: { type: String, required: [true, "Content is required"] },
+    img: { type: String, required: [true, "Img is required"] },
+    owner: { type: ObjectId, ref: "User" },
+    likes: [{ type: ObjectId, ref: "User" }],
+    comments: [{ type: ObjectId, ref: "Comment" }],
+  },
+  { timestamps: { createdAt: "created_at" } }
+);
 
-const Post = model('Post', postSchema);
+const Post = model("Post", postSchema);
 
 module.exports = Post;
