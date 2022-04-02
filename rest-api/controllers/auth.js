0cbca552fe246ac2997b155ac8a51613db46e8f8
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-02 16:01:53 +0300

    build

--- rest-api/controllers/auth.js
+++ rest-api/controllers/auth.js
@@ -1,10 +1,23 @@
 const router = require('express').Router();
 const api = require('../services/user');
 const {cookie_name} = require('../config');
+const  {uploadToCloudinary} = require('../utils/cloudinary');
 
 router.post('/register', async(req, res) => {
     try{
-        const user = await api.register(req.body);
+        const {username, email, password, img, description} = req.body;
+        try{
+            const imgFile = req.body.img;
+            const upload = await uploadToCloudinary(imgFile);
+            return upload;
+        }
+        catch(err){
+            const error = mapErrors(err);
+            res.status(400).send(error);
+        }
+        const user = await api.register({username, email, password, img: upload, description});
+        const token = user.createToken({ id: user._id });
+        res.cookie(cookie_name, token, {httpOnly: true});
         res.status(200).send(user);
     }
     catch(err){
@@ -15,7 +28,11 @@ router.post('/register', async(req, res) => {
 
 router.post('/login', async(req, res) => {
     try{
-        const user = await api.login(req.body);
+        const {email, password} = req.body;
+        const user = await api.login({email, password});
+        const token = user.createToken({ id: user._id });
+        res.cookie(cookie_name, token, {httpOnly: true});
+        res.status(200).send(user);
         res.status(200).send(user);
     }
     catch(err){
