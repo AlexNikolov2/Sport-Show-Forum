ae9783e8d7ae80d9c657111879525f88e9456afa
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-03 12:16:00 +0300

    build

--- sport-show-forum/src/app/shared/shared.module.ts
+++ sport-show-forum/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { TimeDiffPipe } from './pipes/time-diff.pipe';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { NumCountPipe } from './pipes/num-count.pipe';
+import { ErrorComponent } from './error/error.component';
 
 
 
@@ -12,7 +13,8 @@ import { NumCountPipe } from './pipes/num-count.pipe';
     TimeDiffPipe,
     NotFoundComponent,
     ShortenPipe,
-    NumCountPipe
+    NumCountPipe,
+    ErrorComponent
   ],
   imports: [
     CommonModule
