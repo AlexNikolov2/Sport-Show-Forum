5a3c8c71e052ab55fd148446af76ee12552fd76d
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-03 13:13:42 +0300

    temporar, gotta go now

--- sport-show-forum/src/app/post/posts/posts.component.ts
+++ sport-show-forum/src/app/post/posts/posts.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PostService } from 'src/app/core/services/post.service';
+import { IPost } from 'src/app/shared/interface/post';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent {
+
+  posts: IPost[] = [];
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
