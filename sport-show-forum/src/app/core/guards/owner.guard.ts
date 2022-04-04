573813c203c85509c7f65eb68ed2ad78f3e09495
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-04 14:59:11 +0300

    build

--- sport-show-forum/src/app/core/guards/owner.guard.ts
+++ sport-show-forum/src/app/core/guards/owner.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { PostService } from '../services/post.service';
+import { UserService } from '../services/user.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
+
 export class OwnerGuard implements CanActivate {
+  constructor(private userService: UserService,
+    private postService: PostService,
+    private router: Router){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot):boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    const { id: postId } = route.params;
+
+    return this.postService.getPost(postId).pipe(
+      map((post) => {
+       return post.user._id === this.userService.user?._id;
+      })
+    );
   }
-  
 }
