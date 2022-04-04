4c66aa7b6fb8081c8aca2d24d9f037321864958e
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-04 15:25:44 +0300

    build

--- sport-show-forum/src/app/shared/shared.module.ts
+++ sport-show-forum/src/app/shared/shared.module.ts
@@ -5,6 +5,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { NumCountPipe } from './pipes/num-count.pipe';
 import { ErrorComponent } from './error/error.component';
+import { PasswordMatchDirective } from './directives/password-match.directive';
+import { CustomValidatorDirective } from './directives/custom-validator.directive';
 
 
 
@@ -14,7 +16,9 @@ import { ErrorComponent } from './error/error.component';
     NotFoundComponent,
     ShortenPipe,
     NumCountPipe,
-    ErrorComponent
+    ErrorComponent,
+    PasswordMatchDirective,
+    CustomValidatorDirective
   ],
   imports: [
     CommonModule
