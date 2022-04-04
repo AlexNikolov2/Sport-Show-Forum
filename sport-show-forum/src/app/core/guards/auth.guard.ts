cbdbd17c402acd97856458634c9ea23a61b2d128
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-04 14:38:55 +0300

    build

--- sport-show-forum/src/app/core/guards/auth.guard.ts
+++ sport-show-forum/src/app/core/guards/auth.guard.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private UserService: UserService) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let { authRequired } = route.data;
+
+    const previousUrl = this.router.routerState.snapshot.url;
+
+    if (
+      typeof authRequired === 'boolean' &&
+      authRequired == this.UserService.isLoggedIn
+    ) {
+      return true;
+    }
+
+    if (authRequired == true && this.UserService.isLoggedIn == false) {
+      this.router.navigate(['/login'], {
+        queryParams: { redirectTo: previousUrl.slice(1) },
+      });
+      return false;
+    }
+
+    return this.router.parseUrl('/');
   }
-  
 }
