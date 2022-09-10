d28e6a9fde364acaf923486635d5984e7745de94
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-10 18:03:54 +0300

    going for an responsive header

--- sport-show-forum/src/app/core/header/header.component.ts
+++ sport-show-forum/src/app/core/header/header.component.ts
@@ -6,10 +6,23 @@ import { NgModel } from '@angular/forms';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  contentEditable!: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  toggleEditable(event: any) {
+    let output = document.querySelector<HTMLElement>('.sidebarIconToggle')
+    if ( event.target.checked ) {
+        this.contentEditable = true;
+        
+        if (output) output.style.display = 'block';
+   }
+   else{
+    if (output) output.style.display = 'none';
+   }
+   
+}
 
 }
