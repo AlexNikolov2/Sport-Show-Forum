d4391c4c222fb1e52dd17283599102c0638c581f
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-11 14:02:42 +0300

    generating post module components

--- sport-show-forum/src/app/posts/posts.module.ts
+++ sport-show-forum/src/app/posts/posts.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AllPostsComponent,
+    PostDetailsComponent,
+    CreatePostComponent,
+    EditPostComponent
+  ],
   imports: [
     CommonModule
   ]
