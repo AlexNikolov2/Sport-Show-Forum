b2e159b31d07b5a56b8de36a34bed3500868b2e3
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-13 11:26:06 +0300

    eidt post deign implemented

--- sport-show-forum/src/app/posts/posts.module.ts
+++ sport-show-forum/src/app/posts/posts.module.ts
@@ -21,7 +21,9 @@ import { SharedModule } from '../shared/shared.module';
   ],
   exports: [
     AllPostsComponent,
-    CreatePostComponent
+    CreatePostComponent,
+    EditPostComponent,
+    PostDetailsComponent
   ]
 })
 export class PostsModule { }
