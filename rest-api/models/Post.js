1d3da109876ed57e86255e5b8f962564e25d3e5a
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-16 23:12:35 +0300

    post model

--- rest-api/models/Post.js
+++ rest-api/models/Post.js
@@ -1,1 +1,16 @@
 const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    imageURL: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
