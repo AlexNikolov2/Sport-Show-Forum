9aa7799135c8030f352410f4772cb5d1326c9d23
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-18 02:23:08 +0300

    generating a niiice pipe

--- sport-show-forum/src/app/shared/shared.module.ts
+++ sport-show-forum/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostParticleComponent } from './post-particle/post-particle.component';
+import { ShortenPipe } from './pipes/shorten.pipe';
 
 
 
 @NgModule({
   declarations: [
-    PostParticleComponent
+    PostParticleComponent,
+    ShortenPipe
   ],
   imports: [
     CommonModule
