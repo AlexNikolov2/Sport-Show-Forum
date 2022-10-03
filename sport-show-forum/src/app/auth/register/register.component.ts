e731213dfeba153a202f2230cfcdbd60ca4f51d0
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-03 16:39:00 +0300

    eee vaskakena

--- sport-show-forum/src/app/auth/register/register.component.ts
+++ sport-show-forum/src/app/auth/register/register.component.ts
@@ -1,76 +1,35 @@
-import { Component } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Subject } from 'rxjs';
 import { AuthService } from 'src/app/shared/services/auth.service';
-import { auth_success } from 'src/app/shared/store/auth.actions';
-import { GlobalState } from "../../shared/interfaces/global-state";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
-  form: UntypedFormGroup;
-  killSubscription = new Subject<void>();
-  error: string = '';
-  isLoading: boolean = false;
-  
-  constructor(
-    private fb: UntypedFormBuilder,
-    private store: Store<GlobalState>,
-    private authService: AuthService,
-    private router: Router
-  ) {
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+  submitted: boolean = false;
+
+  constructor(public authService: AuthService, private fb: FormBuilder, private router: Router) { }
+
+  ngOnInit(): void {
     this.form = this.fb.group({
-      email: ['', [Validators.required], Validators.email],
-      password: ['', [Validators.required], Validators.minLength(6)],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
-  ngOnDestroy(): void {
-    this.killSubscription.next();
-    this.killSubscription.complete();
-  }
-
-  onSubmit(): void {
-    if (this.form.invalid || this.form.pending) {
-      let message = '';
-      this.form.get('email')?.hasError('required') ? message += `Email is required.` : '';
-      this.form.get('email')?.hasError('invalidEmail') ? message += `\nInvalid email.` : '';
-      this.form.get('password')?.hasError('required') ? message += `\nPassword is required.` : '';
-      this.form.get('password')?.hasError('minLength') ? message += `\nPassword must be at least 8 characters!` : '';
-      this.error = message;
+  onSubmit(){
+    this.submitted = true;
+    if (this.form.invalid) {
       return;
     }
-
-    const email = this.form.value.email;
-    const password = this.form.value.password;
-    this.isLoading = true;
-    this.authService.register({ email, password }).subscribe(
-      user => {
-        this.store.dispatch(auth_success({
-          _id: user._id,
-          email: user.email,
-        }));
-        this.isLoading = false;
-        this.form.reset();
-        this.router.navigateByUrl('/');
-      },
-      error => {
-        this.isLoading = false;
-        if (error.status === 0 || error.status === 500) {
-          this.error = 'Something went wrong. Please try again later.'
-        } else {
-          this.error = error.error.message;
-        }
-      }
-    )
-  }
-
-  onCloseNotification(): void {
-    this.error = '';
+    this.authService.register(this.form.controls['email'].value, /*this.form.controls['password'].value*/);
+    localStorage.setItem('id', 'user')
+    console.log(localStorage);
+    this.form.reset();
+    this.router.navigate(['/all-posts']);
   }
 }
--- sport-show-forum/src/app/shared/interfaces/global-state.ts
+++
@@ -1,5 +0,0 @@
-import { AuthState } from "../store/auth.resolver";
-
-export interface GlobalState {
-    auth: AuthState;
-}
